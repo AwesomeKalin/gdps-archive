dfa041f55f34b8e86e9247b2275decd233940564
AwesomeKalin <47223207+AwesomeKalin@users.noreply.github.com>
2023-10-28 10:49:57 +0100

    Song archiving

--- gdps_archive/archive.py
+++ gdps_archive/archive.py
@@ -2,6 +2,8 @@ import asyncio
 import gd
 import internetarchive
 from os.path import expanduser
+from os import remove
+import urllib.request
 
 async def main(client):
     levelId: int = 7
@@ -24,8 +26,22 @@ async def archive(level, client):
         file.write(lvl.unprocessed_data)
         print('Downloaded Level, starting archive')
         internetarchive.upload('gdps-2.2-level-' + str(level), expanduser("~") + '/.gdpsarchive/' + str(level) + '.gd', metadata={'creator': lvl.creator.name, 'scanner': 'GDPS Editor 2.2 Archiver', 'title': lvl.name, 'subject': 'gdps;geometry dash;2.2;gdps editor 2.2;gdps editor;level', 'description': lvl.description, 'stars': str(lvl.stars), 'difficulty': str(lvl.difficulty.value), 'song': str(lvl.song.id)})
+        print('Level ID ' + level + ' is archived!')
+        remove(str(level), expanduser("~") + '/.gdpsarchive/' + str(level) + '.gd')
+
+        item: internetarchive.Item = internetarchive.get_item('gdps-2.2-song-' + str(lvl.song.id))
+
+        if lvl.song.download_url != None or 'newgrounds' in lvl.song.download_url or item.exists:
+            return True
+        
+        print('Archiving Song ID ' + str(lvl.song.id))
+        urllib.request.urlretrieve(lvl.song.download_url, expanduser("~") + '/.gdpsarchive/' + str(lvl.song.id) + '.mp3')
+        print('Downloaded Song, Archiving')
+        internetarchive.upload('gdps-2.2-song-' + str(lvl.song.id), expanduser("~") + '/.gdpsarchive/' + str(lvl.song.id) + '.mp3', metadata={'creator': lvl.song.artist, 'scanner': 'GDPS Editor 2.2 Archiver', 'title': lvl.song.name, 'subject': 'gdps;geometry dash;2.2;gdps editor 2.2;gdps editor;song', 'description': 'A song archive from the GDPS Editor 2.2 Reupload System. Originally archived for Level ID ' + level})
+        print('Archiving successful!')
+
     except:
-        print('Level does not exist')
+        print('Level does not exist or achiving failed')
     
 client = gd.Client()
 
