ad9f9feaf967c2bf416af9b3dfc702eef7997eb0
AwesomeKalin <47223207+AwesomeKalin@users.noreply.github.com>
2023-10-28 11:05:00 +0100

    Fix deleting

--- gdps_archive/archive.py
+++ gdps_archive/archive.py
@@ -27,7 +27,7 @@ async def archive(level, client):
         print('Downloaded Level, starting archive')
         internetarchive.upload('gdps-2.2-level-' + str(level), expanduser("~") + '/.gdpsarchive/' + str(level) + '.gd', metadata={'creator': lvl.creator.name, 'scanner': 'GDPS Editor 2.2 Archiver', 'title': lvl.name, 'subject': 'gdps;geometry dash;2.2;gdps editor 2.2;gdps editor;level', 'description': lvl.description, 'stars': str(lvl.stars), 'difficulty': str(lvl.difficulty.value), 'song': str(lvl.song.id), 'level-id': level})
         print('Level ID ' + level + ' is archived!')
-        remove(str(level), expanduser("~") + '/.gdpsarchive/' + str(level) + '.gd')
+        remove(expanduser("~") + '/.gdpsarchive/' + str(level) + '.gd')
 
     except:
         print('Level does not exist or archiving failed')
