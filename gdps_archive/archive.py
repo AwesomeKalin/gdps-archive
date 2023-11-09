d0fe7d057c3d60588301d5e9446ac2d4a2930b9e
AwesomeKalin <47223207+AwesomeKalin@users.noreply.github.com>
2023-11-09 16:32:04 +0000

    Fix song archival trying to archive NG songs

--- gdps_archive/archive.py
+++ gdps_archive/archive.py
@@ -7,7 +7,7 @@ import urllib.request
 import yarl
 
 async def main(client):
-    levelId: int = 75
+    levelId: int = 80
 
     print('Starting archive from id ' + str(levelId))
 
@@ -38,13 +38,13 @@ async def archive(level, client):
 
     item: internetarchive.Item = internetarchive.get_item('gdps-2.2-song-' + str(lvl.song.id))
 
-    if lvl.song.download_url == None or 'newgrounds' in lvl.song.download_url.human_repr() or item.exists:
+    if lvl.song.download_url == None or lvl.song.download_url.human_repr().find('newgrounds') != -1 or item.exists:
         return True
   
     print('Archiving Song ID ' + str(lvl.song.id))
     urllib.request.urlretrieve(lvl.song.download_url.human_repr(), expanduser("~") + '/.gdpsarchive/' + str(lvl.song.id) + '.mp3')
     print('Downloaded Song, Archiving')
-    internetarchive.upload('gdps-2.2-song-' + str(lvl.song.id), expanduser("~") + '/.gdpsarchive/' + str(lvl.song.id) + '.mp3', metadata={'creator': lvl.song.artist, 'scanner': 'GDPS Editor 2.2 Archiver', 'title': lvl.song.name, 'subject': 'gdps;geometry dash;2.2;gdps editor 2.2;gdps editor;song', 'description': 'A song archive from the GDPS Editor 2.2 Reupload System. Originally archived for Level ID ' + level, 'collection': 'gdps-editor-2.2', 'mediatype': 'audio'})
+    internetarchive.upload('gdps-2.2-song-' + str(lvl.song.id), expanduser("~") + '/.gdpsarchive/' + str(lvl.song.id) + '.mp3', metadata={'creator': lvl.song.artist, 'scanner': 'GDPS Editor 2.2 Archiver', 'title': lvl.song.name, 'subject': 'gdps;geometry dash;2.2;gdps editor 2.2;gdps editor;song', 'description': 'A song archive from the GDPS Editor 2.2 Reupload System. Originally archived for Level ID ' + str(level), 'collection': 'gdps-editor-2.2', 'mediatype': 'audio'})
     print('Archiving successful!')
     remove(expanduser("~") + '/.gdpsarchive/' + str(lvl.song.id) + '.mp3')
     
