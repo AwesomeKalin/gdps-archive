ac85a4154e4abd023b3f3eaaf21b896efd2cc1a8
AwesomeKalin <47223207+AwesomeKalin@users.noreply.github.com>
2024-01-12 21:19:11 +0000

    Bumped level id

--- gdps_archive/archive.py
+++ gdps_archive/archive.py
@@ -7,7 +7,7 @@ import urllib.request
 import yarl
 
 async def main(client):
-    levelId: int = 434
+    levelId: int = 19535
 
     print('Starting archive from id ' + str(levelId))
 
@@ -43,7 +43,12 @@ async def archive(level, client):
         return True
   
     print('Archiving Song ID ' + str(lvl.song.id))
-    urllib.request.urlretrieve(lvl.song.download_url.human_repr(), expanduser("~") + '/.gdpsarchive/' + str(lvl.song.id) + '.mp3')
+    try:
+        urllib.request.urlretrieve(lvl.song.download_url.human_repr(), expanduser("~") + '/.gdpsarchive/' + str(lvl.song.id) + '.mp3')
+    except:
+        print('Failed to download song')
+        return False
+
     print('Downloaded Song, Archiving')
     internetarchive.upload('gdps-2.2-song-' + str(lvl.song.id), expanduser("~") + '/.gdpsarchive/' + str(lvl.song.id) + '.mp3', metadata={'creator': lvl.song.artist.name, 'scanner': 'GDPS Editor 2.2 Archiver', 'title': lvl.song.name, 'subject': 'gdps;geometry dash;2.2;gdps editor 2.2;gdps editor;song', 'description': 'A song archive from the GDPS Editor 2.2 Reupload System. Originally archived for Level ID ' + str(level), 'collection': 'gdps-editor-2.2', 'mediatype': 'audio'}, verify=True, retries=20)
     print('Archiving successful!')
